33d38bc25453054baa714e4cc5ddec57b2eae3e2
Plasnid <stuart@sackler.ca>
2020-04-13 21:06:12 -0400

    initial file upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/**
+ * *section 1: Arrays, Objects, and Data
+ * 
+ * * Answer 1 of the following questions from section 1
+ */
+
+/**
+ * * Section 1a. Create an object  about an animal that contains:
+ *      * a variable for number of limbs
+ *      * a variable for the animals name
+ *      * a variable saying whether the animal can fly
+ *      * a variable that holds a function
+ *          * the function should return a template literal that says
+ *              * "I am an ANIMAL NAME with NUMBER OF LIMBS limbs"
+ *      * call the the function in your object, and console log the result
+ */
+
+/**
+ *  * Section 1b.  
+ */
